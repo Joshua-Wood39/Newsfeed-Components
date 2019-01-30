5b233af209a03a745bde156fe1a77a8342b33cd7
Joshua Wood <JerichoWall@apu.edu>
2019-01-30 15:36:11 -0800

    MVP and Menu stretch complete--trying to get other things to work

--- Article/Article.js
+++ Article/Article.js
@@ -17,6 +17,8 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
       this.domElement.classList.toggle('article-open');
+      this.expandButton.textContent = 'retract';
+      //TweenMax.to();
   }
 }
 
